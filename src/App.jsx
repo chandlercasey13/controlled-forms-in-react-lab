19947833ae38715d643fa0a49121cab0c1e7d603
Chandler Casey <chandlercasey13@gmail.com>
2024-06-25 15:51:11 -0500

    added bookshelf to display books read

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Bookshelf from "./components/Bookshelf";
+import DisplayBookshelf from "./components/DisplayBookshelf";
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [books, setBooks] = useState([{title: "I'm trash at CSS" , author: 'Chandler'}]);
+
+  const [newBook, setNewBook] = useState({ title: "", author: "" });
+
+  function handleInputChange(event) {
+    setNewBook({ ...newBook, [event.target.name]: event.target.value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    setBooks([ ...books, newBook]);
+    setNewBook({ title: "", author: "" });
+    
+  }
+
+
+  return (
+    <>
+      <h1>My Bookshelf</h1>
+
+      <Bookshelf
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+        newBook={newBook}
+      />
+
+      <DisplayBookshelf 
+      books = {books}
+      
+      />
+    </>
+  );
 };
 
-export default App
+export default App;
---
+++ src/components/DisplayBookshelf.jsx
@@ -0,0 +1,20 @@
+import "./DisplayBookshelf.css";
+const DisplayBookshelf = function ({ books }) {
+  return (
+    <ol value="">
+      <div className="bookCardsDiv">
+        
+          {books.map((book) => (
+            <div className="bookCard">
+            <li>
+              {book.title} by {book.author}
+            </li>
+            </div>
+          ))}
+        
+      </div>
+    </ol>
+  );
+};
+
+export default DisplayBookshelf;
